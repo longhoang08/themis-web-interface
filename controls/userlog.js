2bc4791148b4276d07c614b46bd14b47d2cb87c8
longhoang08 <longhoang0811@gmail.com>
2022-10-29 10:04:46 +0700

    update

--- controls/userlog.js
+++ controls/userlog.js
@@ -102,6 +102,7 @@ function addScore(username, problem, contents) {
 				[`highestScores.submitCounts.${problem}`]: submitCount,
 			}
 		});
+		debug('score after update', user.highestScores[problem]);
 	});
 }
 
